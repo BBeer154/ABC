2cdd1f2269548a974509fdaa12f2bd8a227cd69c
BBeer154 <103546409+BBeer154@users.noreply.github.com>
2023-03-06 16:21:45 +0700

    Create ABCD.lua

---
+++ ABCD.lua
@@ -0,0 +1,4 @@
+if not game:IsLoaded() then repeat game.Loaded:Wait() until game:IsLoaded() end
+  _G.AutoFarm = true
+  }
+loadstring(game:HttpGet('https://raw.githubusercontent.com/lerkermer/lua-projects/master/SwagModeV002'))()
